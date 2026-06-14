f582cf65b595ca38dd31c7e4151164b70a2f9ebd
SalemElbermawy <salemsalemahmed761@gmail.com>
2026-06-14 16:55:08 +0300

    end the front end of the rag_system

---
+++ rag_main.js
@@ -0,0 +1,52 @@
+Url='http://127.0.0.1:8000/model'
+
+async function sendMessage() {
+
+    const=inputField=document.getElementById("user-input");
+    const chatBox = document.getElementById("chat-box");
+    const messageText = inputField.ariaValueMax.trim();
+
+    if (messageText === "") return;
+
+    const userDiv=document.createElement("div");
+    userDiv.className="message user-msg";
+    userDiv.innerHTML=messageText;
+    chatBox.appendChild(userDiv)
+
+    inputField.value="";
+    chatBox.scrollTop=chatBox.scrollHeight;
+
+
+    try{
+        const response = fetch(Url,{
+            method:"POST",
+            headers:{
+                "Content-Type" :"application/json"
+            },
+            body:JSON.stringify({prompt:messageText})
+        });
+
+        const data = (await response).json();
+
+        const botDiv = document.createElement("div");
+        botDiv.className="message bot-msg";
+        botDiv.innerText=data.response;
+        chatBox.appendChild(botDiv);
+    }catch(error){
+        const errorDiv = document.createElement("div");
+        errorDiv.className="message bot-msg";
+        errorDiv.style.color="#ff6b6b";
+        errorDiv.innerHTML="Error We Can't Get The Server";
+        chatBox.appendChild(errorDiv);
+        console.error("Error:",error);
+    }
+    chatBox.scrollTop=chatBox.scrollHeight;
+}
+
+function handleKeyPress(event){
+    if (event.key === "Enter"){
+        sendMessage();
+    }
+}
+
+
